5a20c1a4d67707b5f7604a0b48bc652fdf5df89e
Ville M. Vainio <vivainio@gmail.com>
2012-08-20 10:03:23 +0300

    nicer spreadsheet layout (all starts in col 0)

--- Projects/excel_integration/write_leo_file.py
+++ Projects/excel_integration/write_leo_file.py
@@ -3,39 +3,63 @@
 #@@language python
 
 #@+others
-#@+node:ville.20120817104907.1690: ** content
+#@+node:ville.20120817104907.1690: ** Script to add to your documents
 import xlwt
 import os
 
+@g.command("spreadsheet")
+def spreadsheet_f(event):
+    """ Parse @spreadsheet foo.xls directives """
+    c = event['c']
+    write_spreadsheets(c)
 
-wb = xlwt.Workbook()
 
-ws = wb.add_sheet('Leo dump')
-
-root = c.find_h("@spreadsheet.*")
-
-#fname ='leo_test.xls'
-g.es( root)
-
-p = root[0]
-
-row = 0
-
-for n in p.subtree():    
-    col=n.level()
-    ws.write(row, col, n.h)
-    ws.write(row, col+1, n.b.lstrip())
-    row += 1
-
-os.remove("example.xls")
-
-wb.save('example.xls')
-
-g.os_startfile("example.xls")
+def write_spreadsheets(c):
     
-g.es(n)
+    wb = xlwt.Workbook()
     
+    font0 = xlwt.Font()
+    font0.name = 'Times New Roman'
+    #font0.colour_index = 2
+    font0.bold = True
     
+    style0 = xlwt.XFStyle()
+    
+    style1 = xlwt.XFStyle()
+    style1.font = font0
+        
+    ws = wb.add_sheet('Leo dump')
+    
+    roots = c.find_h("@spreadsheet.*")
+    for root in roots:
+        parts = root.h.split(None,1)
+        if len(parts) == 2:
+            fname = parts[1]
+            pth = c.getNodePath(root)
+            fname = pth + "/" + fname
+        #fname ='leo_test.xls'
+        g.es("Spreadsheet: " + fname)
+        
+        row = 0
+        
+        for n in root.subtree():    
+            col=0            
+            if n.v.children:
+        
+                ws.write(row, col, n.h,style1)
+            else:
+                ws.write(row, col, n.h, style0)
+            ws.write(row, col+1, n.b.strip())
+            row += 1
+        
+        #if os.path.exists(
+        #os.remove(fname)
+        
+        wb.save(fname)
+        g.es("Wrote " + fname)
+        
+        g.os_startfile(fname)
 
+write_spreadsheets(c)
 #@-others
 #@-leo
